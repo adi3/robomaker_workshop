e5ea06da44c7c743411bdffbc862aeae6f478aa3
Adi Singh <1301549+adi3@users.noreply.github.com>
2021-06-14 18:05:26 +0200

    Update fetch_dataset.sh

--- fetch_dataset.sh
+++ fetch_dataset.sh
@@ -9,4 +9,4 @@ BUCKET=$(aws s3 ls | awk '{print $3}' | grep "custom-labels-console")
 aws s3 cp /tmp/px100-dataset s3://$BUCKET/assets/px100-dataset --recursive
 
 NUM=$(aws s3 ls s3://$BUCKET/assets/px100-dataset/ | wc -l)
-echo "Uploaded $NUM images to $BUCKET bucket on S3"
+echo "SUCCESS: Uploaded $NUM images to $BUCKET on S3"
