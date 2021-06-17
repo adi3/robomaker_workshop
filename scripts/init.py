2972569214ca6f708079fadeecca510c34133028
Adi Singh <adisin8@gmail.com>
2021-06-17 11:22:45 +0000

    Added sim flag to init script

--- scripts/init.py
+++ scripts/init.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
+import sys
 import rospy
 from px100 import PX100
 
 
 def main():
+  # Sim option will use move_it to drive arm in Gazebo
+  # Otherwise script will attempt to move physical arm
+  _sim = False
+  if len(sys.argv) > 1:
+    if sys.argv[1] == "--sim":
+      _sim = True
+      
   rospy.init_node("init", anonymous=True)
-  robot = PX100()
+  robot = PX100(simulated = _sim)
     
   try:
     robot.home()
