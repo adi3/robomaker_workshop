66d74fff8f8754d62293ab8631b59398aa995a98
Adi Singh <1301549+adi3@users.noreply.github.com>
2021-06-17 13:38:19 +0200

    Update init.py

--- scripts/init.py
+++ scripts/init.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 
+import sys
 import rospy
 from px100 import PX100
 
 
 def main():
-  rospy.init_node("init", anonymous=True)
-  robot = PX100()
+  # Sim option will use move_it to drive arm in Gazebo
+  # Otherwise script will attempt to move physical arm
+  _sim = False
+  if len(sys.argv) > 1:
+    if sys.argv[1] == "--sim":
+      _sim = True
     
   try:
+    robot = PX100(simulated = _sim)
+    
     robot.home()
-    if robot.sleep():
-      rospy.loginfo("Initialized robot to sleep position")
-    else:
-      rospy.loginfo("Failed to initialize robot correctly")
+    robot.sleep()
+    rospy.loginfo("Robot initialization successful")
     
   except rospy.ROSInterruptException:
     return
