16c82b6aeb188808dde7b0a67c59222b25f6884c
Adi Singh <1301549+adi3@users.noreply.github.com>
2022-06-21 03:08:59 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export DISPLAY=:0
+#export DISPLAY=:0
 
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt upgrade -yq
@@ -16,10 +16,6 @@ mkdir -p ~/aws_ws/src/
 cd ~/aws_ws/src/
 git clone https://github.com/adi3/robomaker_workshop
 
-cd ~/aws_ws
-rosdep update
-rosdep install --from-paths src --ignore-src --rosdistro melodic -r -y
-
 cd ~/
 curl 'https://raw.githubusercontent.com/Interbotix/interbotix_ros_manipulators/main/interbotix_ros_xsarms/install/rpi4/xsarm_rpi4_install.sh' > xsarm_rpi4_install.sh
 chmod +x xsarm_amd64_install.sh
@@ -27,6 +23,8 @@ echo "N" | ./xsarm_amd64_install.sh
 rm xsarm_amd64_install.sh
 
 cd ~/aws_ws
+rosdep update
+rosdep install --from-paths src --ignore-src --rosdistro melodic -r -y
 catkin build
 
 #echo "export SVGA_VGPU10=0" >> ~/.bashrc
@@ -35,6 +33,6 @@ echo "source ~/aws_ws/devel/setup.bash" >> ~/.bashrc
 echo "export ROS_PACKAGE_PATH=/home/ubuntu/interbotix_ws/src:$ROS_PACKAGE_PATH" >> ~/.bashrc
 echo "export PYTHONPATH=/home/ubuntu/interbotix_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH" >> ~/.bashrc
 
-echo "[[ -f ~/.bashrc ]] && source ~/.bashrc" >> ~/.bash_profile
-source ~/.bash_profile
+#echo "[[ -f ~/.bashrc ]] && source ~/.bashrc" >> ~/.bash_profile
+#source ~/.bash_profile
 #sudo reboot
